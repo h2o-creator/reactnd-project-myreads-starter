a773dff4c608aeaa67aa58bc38b9ece8e3399910
h2o-creator <hadysalah1455@gmail.com>
2021-10-13 18:14:24 +0200

    Implement change shelf in shelf list

--- src/CreateShelf.js
+++ src/CreateShelf.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import ChangeShelf from './ChangeShelf';
 
 export default class CreateShelf extends Component {
     static propTypes = {
         shelfName: PropTypes.string.isRequired,
-        shelfBooks: PropTypes.array.isRequired
+        shelfBooks: PropTypes.array.isRequired,
+        onChangeShelf: PropTypes.func.isRequired
     }
     render() {
-        const { shelfName, shelfBooks } = this.props;
+        const { shelfName, shelfBooks, onChangeShelf } = this.props;
         return (
             <div className="bookshelf">
                 <h2 className="bookshelf-title">{shelfName}</h2>
@@ -17,16 +19,8 @@ export default class CreateShelf extends Component {
                         <li key={book.id}>
                             <div className="book">
                                 <div className="book-top">
-                                <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
-                                <div className="book-shelf-changer">
-                                    <select>
-                                    <option value="move" disabled>Move to...</option>
-                                    <option value="currentlyReading">Currently Reading</option>
-                                    <option value="wantToRead">Want to Read</option>
-                                    <option value="read">Read</option>
-                                    <option value="none">None</option>
-                                    </select>
-                                </div>
+                                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                                    <ChangeShelf book={book} shelf={book.shelf} onChangeShelf={onChangeShelf} />
                                 </div>
                                 <div className="book-title">{book.title}</div>
                                 <div className="book-authors">{book.authors.map((author) => (<div key={author}>{author}</div>))}</div>
