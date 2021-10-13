b8b87c251679034c360b924346c3c211ec5c8440
h2o-creator <hadysalah1455@gmail.com>
2021-10-13 18:13:54 +0200

    Add change shelf code

---
+++ src/ChangeShelf.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+export default class ChangeShelf extends Component {
+    static propTypes = {
+        book: PropTypes.object.isRequired,
+        shelf: PropTypes.string.isRequired,
+        onChangeShelf: PropTypes.func.isRequired
+    }
+
+    tryChangeShelf = (event) => {
+        const { book, onChangeShelf } = this.props;
+        onChangeShelf(book, event.target.value);
+    }
+
+    render() {
+        const { shelf } = this.props;
+        return (
+            <div className="book-shelf-changer">
+                <select value={shelf} onChange={this.tryChangeShelf} >
+                    <optgroup label="Move to...">
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </optgroup>
+                </select>
+            </div>
+        )
+    }
+}
