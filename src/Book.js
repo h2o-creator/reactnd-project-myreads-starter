01ec8a6aaef907d4225d3b6cdfc7e14011099a01
h2o-creator <hadysalah1455@gmail.com>
2021-10-13 22:37:48 +0200

    Use get shelf callback instead of passing prop

--- src/Book.js
+++ src/Book.js
@@ -3,14 +3,14 @@ import PropTypes from 'prop-types'
 import ChangeShelf from './ChangeShelf'
 
 const Book = (props) => {
-    const { id, image, title, shelf, onChangeShelf, authors } = props;
+    const { id, image, title, onChangeShelf, getBookShelf, authors } = props;
     return (
         <li>
             <div className="book">
                 <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${image})` }}></div>
                     {(onChangeShelf) && (
-                        <ChangeShelf book={id} shelf={shelf} onChangeShelf={onChangeShelf} />
+                        <ChangeShelf book={id} onChangeShelf={onChangeShelf} getBookShelf={getBookShelf} />
                     )}
                 </div>
                 <div className="book-title">{title}</div>
@@ -24,9 +24,10 @@ Book.propTypes = {
     id: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    shelf: PropTypes.string.isRequired,
-    authors: PropTypes.array.isRequired,
-    onChangeShelf: PropTypes.func
+    authors: PropTypes.array,
+    onChangeShelf: PropTypes.func.isRequired,
+    isBookInMainArray: PropTypes.func,
+    getBookShelf: PropTypes.func.isRequired
 }
 
 export default Book
