055173a5016ee8dfd41e97aae30f1dc5c01fe2a2
h2o-creator <hadysalah1455@gmail.com>
2021-10-14 09:14:23 +0200

    Beautify code and fix thumbnail error

--- src/CreateShelf.js
+++ src/CreateShelf.js
@@ -2,13 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Book from './Book'
 
-export default class CreateShelf extends Component {
-    static propTypes = {
-        shelfName: PropTypes.string.isRequired,
-        shelfBooks: PropTypes.array.isRequired,
-        onChangeShelf: PropTypes.func.isRequired,
-        getBookShelf: PropTypes.func.isRequired
-    }
+export default class CreateShelf extends Component { 
     render() {
         const { shelfName, shelfBooks, onChangeShelf } = this.props;
         return (
@@ -17,11 +11,20 @@ export default class CreateShelf extends Component {
                 <div className="bookshelf-books">
                 <ol className="books-grid">
                     {shelfBooks.map((book) => (
-                        <Book key={book.id} id={book.id} image={book.imageLinks.thumbnail} title={book.title} shelf={book.shelf} authors={book.authors} onChangeShelf={onChangeShelf} getBookShelf={this.props.getBookShelf} />
+                        <Book key={book.id} id={book.id} 
+                        image={book.imageLinks !== undefined ? book.imageLinks.thumbnail : 'https://upload.wikimedia.org/wikipedia/en/6/60/No_Picture.jpg'} 
+                        title={book.title} shelf={book.shelf} authors={book.authors} onChangeShelf={onChangeShelf} getBookShelf={this.props.getBookShelf} />
                     ))}
                 </ol>
                 </div>
             </div>
         )
     }
+}
+
+CreateShelf.propTypes = {
+    shelfName: PropTypes.string.isRequired,
+    shelfBooks: PropTypes.array.isRequired,
+    onChangeShelf: PropTypes.func.isRequired,
+    getBookShelf: PropTypes.func.isRequired
 }
